95fa087df1d9599828eba1545cc018a40e053855
Kovács, Péter <peter.dunay.kovacs@gmail.com>
2017-04-14 16:55:58 +0200

    Provide more checker to choose from
    - make MyPublicIpChecker abstract
    - add ‘whatismypublicip.com’ as a checker

--- src/backend/MyPublicIpChecker.ts
+++ src/backend/MyPublicIpChecker.ts
@@ -2,16 +2,19 @@ import * as Immutable from 'immutable';
 import * as https from 'https';
 import { ClientRequest, IncomingMessage } from 'http';
 
-const requestOptions = {
-    protocol: 'https:',
-    host: 'ipinfo.io',
-    path: '/ip',
-    method: 'GET'
-};
-
 const NO_CHANGE = '__nochange__';
 
-export class MyPublicIpChecker {
+export const createMyPublicIpChecker : (key : string, initalIp : string) => MyPublicIpChecker =
+    (key, initalIp) : MyPublicIpChecker => {
+        switch(key) {
+            case 'ipinfo.io':
+                return new IpInfoIoChecker(initalIp);
+            case 'whatismypublicip.com':
+                return new WhatIsMyPublicIpCom(initalIp);
+        }
+    }
+
+export abstract class MyPublicIpChecker {
 
     private lastIp : string;
 
@@ -19,18 +22,22 @@ export class MyPublicIpChecker {
         this.lastIp = initialIp;
     }
 
+    abstract getRequestOptions() : https.RequestOptions;
+
+    abstract parseOutput(output : string) : string;
+
     public check() : Promise<string> {
 
         return new Promise((resolve, reject) => {
 
-            let request = https.request(requestOptions, (response) => {
+            let request = https.request(this.getRequestOptions(), (response) => {
 
                 let output : string = '';
 
                 response.on('data', (chunk) => {
                     output += chunk;
                 }).on('end', () => {
-                    let currentIp = output.trim();
+                    let currentIp = this.parseOutput(output);
                     if (currentIp === this.lastIp) {
                         resolve(NO_CHANGE);
                     } else {
@@ -52,4 +59,38 @@ export class MyPublicIpChecker {
     public static isNewIp(maybeNewIp : string) : boolean {
         return maybeNewIp !== NO_CHANGE;
     }
+}
+
+class IpInfoIoChecker extends MyPublicIpChecker {
+
+    getRequestOptions(): https.RequestOptions {
+        return {
+            protocol: 'https:',
+            host: 'ipinfo.io',
+            path: '/ip',
+            method: 'GET'
+        };
+    }
+
+    parseOutput(output: string): string {
+        return output.trim();
+    }
+
+}
+
+class WhatIsMyPublicIpCom extends MyPublicIpChecker {
+
+    getRequestOptions(): https.RequestOptions {
+        return {
+            protocol: 'https:',
+            host: 'whatismypublicip.com',
+            method: 'GET'
+        };
+    }
+
+    parseOutput(output: string): string {
+        let re = /<div id="up_finished">(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s*<\/div>/;
+        return output.replace(re, '$1');
+    }
+
 }
--- src/backend/noip-updater.ts
+++ src/backend/noip-updater.ts
@@ -2,13 +2,18 @@ import * as Process from 'process';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as Immutable from 'immutable';
-import { MyPublicIpChecker } from './MyPublicIpChecker';
+import { MyPublicIpChecker, createMyPublicIpChecker } from './MyPublicIpChecker';
 import { NoIpUpdater } from './NoIpUpdater';
 
 interface Configuration {
-    initialIp: string,
-    checkIntervall : number,
-    auth : string
+    checker: {
+        initialIp: string,
+        checkIntervall : number,
+        impl: string
+    },
+    updater: {
+        auth : string
+    }
 }
 
 const myHostnames = Immutable.List.of('bitkitchen.org', 'mail.bitkitchen.org', 'www.bitkitchen.org');
@@ -16,8 +21,8 @@ const myHostnames = Immutable.List.of('bitkitchen.org', 'mail.bitkitchen.org', '
 const getConfigFilePath : () => string = () => path.resolve(Process.env['HOME'], '.bitkitchen.org/noip-config.json');
 const configuration : Configuration = (() => JSON.parse(fs.readFileSync(getConfigFilePath(), { encoding: 'utf8' })))();
 
-let checker : MyPublicIpChecker = new MyPublicIpChecker(configuration.initialIp);
-let updater : NoIpUpdater = new NoIpUpdater(configuration.auth);
+let checker : MyPublicIpChecker = createMyPublicIpChecker(configuration.checker.impl, configuration.checker.initialIp);
+let updater : NoIpUpdater = new NoIpUpdater(configuration.updater.auth);
 
 const updateFirstHost : (hostnameList : Immutable.List<string>) => Promise<boolean> =
     (hostnameList) => {
@@ -48,7 +53,7 @@ const chainedCheck : () => void = () => {
     }).then((isToBeContinued) => {
         console.log(`isToBeContinued=${isToBeContinued}`);
         if (isToBeContinued) {
-            setTimeout(chainedCheck, configuration.checkIntervall);
+            setTimeout(chainedCheck, configuration.checker.checkIntervall);
         }
     });
 }
